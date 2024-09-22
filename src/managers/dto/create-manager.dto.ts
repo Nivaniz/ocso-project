4d8c509ecb8b234bae97b9fcb5d0716626a39fdd
Nivaniz <nivaniz2019@gmail.com>
2024-09-21 20:07:20 -0600

    Vídeo 30

--- src/managers/dto/create-manager.dto.ts
+++ src/managers/dto/create-manager.dto.ts
@@ -1,1 +1,16 @@
-export class CreateManagerDto {}
+import { IsEmail, IsNumber, IsString, MaxLength } from "class-validator"
+import { Manager } from "../entities/manager.entity"
+
+export class CreateManagerDto extends Manager {
+    @IsString()
+    @MaxLength(80)
+    managerFullName: string
+    @IsString()
+    @IsEmail()
+    managerEmail: string
+    @IsString()
+    @MaxLength(16)
+    managerPhoneNumber: string
+    @IsNumber()
+    managerSalary: number;
+}
