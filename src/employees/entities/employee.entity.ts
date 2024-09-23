52229bc2250bee50f45bec0ed37dfdc0551e3c30
Nivaniz <nivaniz2019@gmail.com>
2024-09-23 07:49:29 -0600

    Added relation to entities

--- src/employees/entities/employee.entity.ts
+++ src/employees/entities/employee.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Location } from "src/locations/entities/location.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Employee {
@@ -17,4 +18,7 @@ export class Employee {
         nullable: true,
     })
     photoUrl: string;
+
+    @ManyToOne(() => Location, (location) => location.employees)
+    location: Location;
 }
