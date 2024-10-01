db39aaf01c40745b4821f9e7d14e897534d8060e
Nivaniz <nivaniz2019@gmail.com>
2024-09-30 23:17:06 -0600

    Video 46

--- src/managers/entities/manager.entity.ts
+++ src/managers/entities/manager.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { User } from "src/auth/entities/user.entity";
 import { Location } from "src/locations/entities/location.entity";
 import { Column, Entity, JoinColumn, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
@@ -6,14 +7,30 @@ import { Column, Entity, JoinColumn, OneToMany, OneToOne, PrimaryGeneratedColumn
 export class Manager {
     @PrimaryGeneratedColumn('uuid')
     managerId: string;
+
+    @ApiProperty({
+        default: "Juan"
+    })
     @Column('text')
     managerFullName: string;
+
+    @ApiProperty({
+        default: "2000"
+    })
     @Column('float')
     managerSalary: number;
+
+    @ApiProperty({
+        default: "manager@gmail.com"
+    })
     @Column('text', {
         unique: true,
     })
     managerEmail: string;
+
+    @ApiProperty({
+        default: "4426544372"
+    })
     @Column('text')
     managerPhoneNumber: string;
 
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Provider } from "src/providers/entities/provider.entity";
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
 
@@ -5,12 +6,25 @@ import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
 export class Product {
     @PrimaryGeneratedColumn("uuid")
     productId: string;
+
+    @ApiProperty({
+        default: "Refresco Patito"
+    })
     @Column({type: "text"})
     productName: string;
+
+    @ApiProperty({
+        default: "29"
+    })
     @Column({type: "float"})
     price: number;
+
+    @ApiProperty({
+        default: "2"
+    })
     @Column({type: "int"})
     countSeal: number;
+    
     @ManyToOne(() => Provider, (provider) => provider.products, {
         eager: true,
     })
