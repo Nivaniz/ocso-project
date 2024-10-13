280b5e77e72d3b70cd3580719b2ee05b91ab4b88
Nivaniz <nivaniz2019@gmail.com>
2024-10-13 13:10:30 -0600

    Vídeo 66

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: "localhost:3000"
+    }
+  });
   const config = new DocumentBuilder()
     .setTitle('OXXO API')
     .setDescription('API for oxxo managment')
@@ -20,6 +24,6 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true,
   }))
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
