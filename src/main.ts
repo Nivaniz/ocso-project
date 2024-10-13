3d2e8b6c12fcac61a57862a7afc171f0be410271
Nivaniz <nivaniz2019@gmail.com>
2024-10-13 13:47:04 -0600

    Vídeo login

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: "localhost:3000"
+      origin: "http://localhost:3000"
     }
   });
   const config = new DocumentBuilder()
