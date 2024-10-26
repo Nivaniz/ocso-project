0f84c54cdfe14d978d67ffe53d81e883eb737df5
Nivaniz <nivaniz2019@gmail.com>
2024-10-26 13:00:28 -0600

    Vídeo 93

--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -4,12 +4,15 @@ import { UpdateLocationDto } from './dto/update-location.dto';
 import { Repository } from 'typeorm';
 import { Location } from './entities/location.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Manager } from 'src/managers/entities/manager.entity';
 
 @Injectable()
 export class LocationsService {
   constructor(
     @InjectRepository(Location)
-    private locationRepository: Repository<Location>
+    private locationRepository: Repository<Location>,
+    @InjectRepository(Manager)
+    private managerRepository: Repository<Manager>
   ){}
 
   create(createLocationDto: CreateLocationDto) {
@@ -29,12 +32,24 @@ export class LocationsService {
   }
 
   async update(id: number, updateLocationDto: UpdateLocationDto) {
+    // Set manager to null
+    this.managerRepository.createQueryBuilder().update().set({ location: null }).where("locationId = :id", {
+      id,
+    }).execute();
+
     const location = await this.locationRepository.preload({
       locationId: id,
       ...updateLocationDto, // Aquí está la corrección
     });
-    return this.locationRepository.save(location);
-}
+    const savedLocation = await this.locationRepository.save(location);
+
+    const updatedManager = await this.managerRepository.preload({
+      managerId: updateLocationDto.manager,
+      location: location,
+    })
+    this.managerRepository.save(updatedManager);
+    return savedLocation;
+  }
 
   remove(id: number) {
     return this.locationRepository.delete({
--- src/managers/entities/manager.entity.ts
+++ src/managers/entities/manager.entity.ts
@@ -35,6 +35,9 @@ export class Manager {
     managerPhoneNumber: string;
 
     @OneToOne(() => Location)
+    @JoinColumn({
+        name: "locationId"
+    })
     location: Location;
     
     @OneToOne(() => User)
